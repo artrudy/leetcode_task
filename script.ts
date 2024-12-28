ed9a7c9af471a843a035e58893c29a159d8e7351
artёm rudoy <59513108+artrudy@users.noreply.github.com>
2024-12-28 19:48:32 +0200

    Add files via upload
    412. Fizz Buzz
    faster solution using switch-case

--- script.ts
+++ script.ts
@@ -1,13 +1,19 @@
 function fizzBuzz(n: number): string[] {
   let arr: string[] = [];
   for (let i = 1; i <= n; i += 1) {
-    if (i % 3 === 0 && i % 5 === 0) {
-      arr.push("FizzBuzz");
-    } else if (i % 3 === 0) {
-      arr.push("Fizz");
-    } else if (i % 5 === 0) {
-      arr.push("Buzz");
-    } else arr.push(`${i}`);
+    switch (true) {
+      case i % 3 === 0 && i % 5 === 0:
+        arr.push("FizzBuzz");
+        break;
+      case i % 3 === 0:
+        arr.push("Fizz");
+        break;
+      case i % 5 === 0:
+        arr.push("Buzz");
+        break;
+      default:
+        arr.push(`${i}`);
+    }
   }
   return arr;
 }
