6647282c07d4a721e89b6095b71ebfe31e6d41e8
artёm rudoy <59513108+artrudy@users.noreply.github.com>
2024-12-28 19:52:17 +0200

    412.ts

---
+++ 412.ts
@@ -0,0 +1,19 @@
+function fizzBuzz(n: number): string[] {
+  let arr: string[] = [];
+  for (let i = 1; i <= n; i += 1) {
+    switch (true) {
+      case i % 3 === 0 && i % 5 === 0:
+        arr.push("FizzBuzz");
+        break;
+      case i % 3 === 0:
+        arr.push("Fizz");
+        break;
+      case i % 5 === 0:
+        arr.push("Buzz");
+        break;
+      default:
+        arr.push(`${i}`);
+    }
+  }
+  return arr;
+};
